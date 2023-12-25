4cd8f21099128a26ec1fdb14ae31963d3a974f15
Fabian Wey <fabian.wey@students.ffhs.ch>
2023-12-25 10:16:48 +0100

    Layout mit 3 Spalten

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from 'src/app/header/header.component';
+import { InfopanelComponent } from './infopanel/infopanel.component';
+import { BoardComponent } from './board/board.component';
+import { CardgridComponent } from './board/cardgrid/cardgrid.component';
+import { Player1handComponent } from './board/player1hand/player1hand.component';
+import { Player2handComponent } from './board/player2hand/player2hand.component';
+import {ChatpanelComponent} from "./chatpanel/chatpanel.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ChatpanelComponent,
+    InfopanelComponent,
+    BoardComponent,
+    CardgridComponent,
+    Player1handComponent,
+    Player2handComponent,
+    ChatpanelComponent
   ],
   imports: [
     BrowserModule
